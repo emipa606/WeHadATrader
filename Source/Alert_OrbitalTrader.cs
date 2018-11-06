5f9341a6420b1dd74af2b3a318a30be1abc9cbb3
Mlie <emipa606@users.noreply.github.com>
2018-11-06 19:24:44 +0100

    Updated source

---
+++ Source/Alert_OrbitalTrader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace RimWorld
+{
+    public class Alert_OrbitalTrader : Alert
+    {
+        /// <summary>
+        /// Returns ticks in human readable time left
+        /// </summary>
+        /// <param name="ticks"></param>
+        /// <returns></returns>
+        private string ticksToHumanTime(int ticks)
+        {
+            if (ticks < 2500)
+                return "less than an hour";
+            var hours = Math.Ceiling((decimal)(ticks / 2500));
+            if (hours == 1) return "about an hour";
+            return hours + " hours";
+        }
+
+        public override string GetLabel()
+        {
+            foreach (Map map in Find.Maps)
+                if (map.passingShipManager.passingShips.Count > 1)
+                    return string.Format("OrbitalTrader".Translate(), "s");
+            return string.Format("OrbitalTrader".Translate(), "");
+        }
+
+        public override string GetExplanation()
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (Map map in Find.Maps)
+                foreach (PassingShip ship in map.passingShipManager.passingShips)
+                {
+                    stringBuilder.AppendLine("    " + ship.FullTitle);
+                    stringBuilder.AppendLine("    Leaves in " + ticksToHumanTime(ship.ticksUntilDeparture));
+                }
+            return string.Format("OrbitalTraderDesc".Translate(), stringBuilder.ToString());
+        }
+
+        public override AlertReport GetReport()
+        {
+            foreach (Map map in Find.Maps)
+            {
+                if (map.passingShipManager.passingShips.Count > 0)
+                {
+                    Building_CommsConsole console = map.listerBuildings.AllBuildingsColonistOfClass<Building_CommsConsole>().FirstOrDefault();
+                    if (console != null)
+                        return AlertReport.CulpritIs(console);
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Source/Alert_PawnTrader.cs
@@ -0,0 +1,48 @@
+﻿using RimWorld.Planet;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace RimWorld
+{
+    public class Alert_PawnTrader : Alert
+    {
+        
+        private IEnumerable<Pawn> TraderPawns
+        {
+            get
+            {
+                return PawnsFinder.AllMaps_Spawned.Where(p =>
+                {
+                    return (p.trader != null && p.trader.CanTradeNow);
+                });
+            }
+        }
+        
+        public override string GetLabel()
+        {
+            if (TraderPawns.Count() > 1)
+                return string.Format("PawnTrader".Translate(), "s");
+            return string.Format("PawnTrader".Translate(), "");
+        }
+        public override string GetExplanation()
+        {
+            var stringBuilder = new StringBuilder();
+            foreach(Pawn pawn in TraderPawns)
+                stringBuilder.AppendLine("    " + pawn.Faction.Name);
+
+            return string.Format("PawnTraderDesc".Translate(), stringBuilder.ToString());
+        }
+
+        public override AlertReport GetReport()
+        {
+            Pawn pawn = TraderPawns.FirstOrDefault();
+            if (pawn == null)
+                return false;
+            return AlertReport.CulpritIs((GlobalTargetInfo)((Thing)pawn));
+        }
+
+        
+    }
+}
