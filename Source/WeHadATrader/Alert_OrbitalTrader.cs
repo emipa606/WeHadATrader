6b826186f856bb909f18a98acda4b42d1a2cb8df
emipa606 <emil.offermo@litet.net>
2020-10-07 13:35:12 +0200

    Added support for Trader ships-mod

---
+++ Source/WeHadATrader/Alert_OrbitalTrader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace RimWorld
+{
+    public class Alert_OrbitalTrader : Alert
+    {
+
+        /// <summary>
+        /// Returns ticks in human readable time left
+        /// </summary>
+        /// <param name="ticks"></param>
+        /// <returns></returns>
+        public static string TicksToHumanTime(int ticks)
+        {
+            if (ticks < 2500)
+                return "less than an hour";
+            var hours = Math.Ceiling((decimal)(ticks / 2500));
+            if (hours == 1) return "about an hour";
+            return hours + " hours";
+        }
+
+        public override string GetLabel()
+        {
+            foreach (Map map in Find.Maps)
+                if (map.passingShipManager.passingShips.Count > 1)
+                    return "OrbitalTraderMulti".Translate();
+            return "OrbitalTraderSingle".Translate();
+        }
+
+        public override TaggedString GetExplanation()
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (Map map in Find.Maps)
+                foreach (PassingShip ship in map.passingShipManager.passingShips)
+                {
+                    stringBuilder.AppendLine(ship.FullTitle);
+                    stringBuilder.AppendLine("Leaves in " + TicksToHumanTime(ship.ticksUntilDeparture));
+                }
+            return string.Format("OrbitalTraderDesc".Translate(), stringBuilder.ToString());
+        }
+
+        public override AlertReport GetReport()
+        {
+            foreach (Map map in Find.Maps)
+            {
+                if (map.passingShipManager.passingShips.Count > 0)
+                {
+                    Building_CommsConsole console = map.listerBuildings.AllBuildingsColonistOfClass<Building_CommsConsole>().FirstOrDefault();
+                    if (console != null)
+                        return AlertReport.CulpritIs(console);
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Source/WeHadATrader/Alert_PawnTrader.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace RimWorld
+{
+    public class Alert_PawnTrader : Alert
+    {
+        private List<Building> TraderShips
+        {
+            get
+            {
+                var ships = new List<Building>();
+                try
+                {
+                    var buildings = Find.CurrentMap?.listerBuildings?.allBuildingsNonColonist;
+                    foreach (var building in buildings)
+                    {
+                        if (building.def.defName == "TraderShipsShip")
+                        {
+                            ships.Add(building);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                }
+                return ships;
+            }
+        }
+
+        private IEnumerable<Pawn> TraderPawns
+        {
+            get
+            {
+                return PawnsFinder.AllMaps_Spawned.Where(p =>
+                {
+                    return p.CanTradeNow;
+                });
+            }
+        }
+        
+        public override string GetLabel()
+        {
+            if (TraderPawns.Count() + TraderShips.Count() > 1)
+                return "PawnTraderMulti".Translate();
+            return "PawnTraderSingle".Translate();
+        }
+        public override TaggedString GetExplanation()
+        {
+            var listOfTraders = new List<TaggedString>();
+            foreach (Pawn pawn in TraderPawns)
+                listOfTraders.Add(pawn.NameFullColored + ", " + pawn.Faction.NameColored);
+
+            foreach (Building buildning in TraderShips)
+                listOfTraders.Add("Shipfrom".Translate() + buildning.Label + Environment.NewLine + buildning.GetInspectString());
+            TaggedString returnString = string.Join(Environment.NewLine + Environment.NewLine, listOfTraders);
+            return "PawnTraderDesc".Translate().Replace("{0}", "") + returnString;
+        }
+
+        public override AlertReport GetReport()
+        {
+            if (TraderPawns.Count() == 0 && TraderShips.Count() == 0) { return false; }
+
+            var targetList = new List<Thing>();
+            foreach (var trader in TraderPawns)
+            {
+                targetList.Add(trader);
+            }
+            foreach (var ship in TraderShips)
+            {
+                targetList.Add(ship);
+            }
+
+            return AlertReport.CulpritsAre(targetList);
+        }
+
+
+    }
+}
